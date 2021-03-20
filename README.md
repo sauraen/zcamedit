de9d36589008deca7a89737ffe75adcf43242218
Sauraen <sauraen@gmail.com>
2021-03-19 18:15:30 -0700

    Updated camera motion

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 zcamedit - Zelda 64 (OoT/MM) cutscene camera editor Blender plugin
+
 Copyright (C) 2021 Sauraen
+
 GPL V3 licensed, see LICENSE.txt
 
 This is a Blender plugin for 2.80+. Copy the zcamedit directory to your Blender plugins directory, then start Blender, go to Preferences > Addons, and search for and enable zcamedit.
--- zcamedit/CamMotion.py
+++ zcamedit/CamMotion.py
@@ -1,20 +1,39 @@
 import bpy
+import math, mathutils
 from bpy.app.handlers import persistent
 
-def GetCutsceneCamera(scene):
+def GetCutsceneCameras(scene):
+    ret = []
     for o in scene.objects:
-        if o.type == 'CAMERA' and o.name == 'CutsceneCam':
-            return o
-    return None
+        if o.type != 'CAMERA': continue
+        if o.parent is None: continue
+        if o.parent.type != 'EMPTY': continue
+        if not o.parent.name.startswith('Cutscene.'): continue
+        ret.append(o)
+    return ret
+
+
+def GetCutsceneCamState(cso, frame):
+    '''Returns (pos, rot_quat, fov)'''
+    return (mathutils.Vector((0.0, frame * 0.1, 0.0)),
+        mathutils.Quaternion(), 45.0)
+    
 
 @persistent
 def CamMotionFrameHandler(scene):
-    print('Frame Change', scene.frame_current)
-    camo = GetCutsceneCamera(scene)
-    if camo is None:
-        return
-    cam = camo.data
-    
+    cams = GetCutsceneCameras(scene)
+    print('Frame Change', scene.frame_current, len(cams), 'eligible cameras')
+    for camo in cams:
+        cso = camo.parent
+        cam = camo.data
+        pos, rot_quat, fov = GetCutsceneCamState(cso, scene.frame_current)
+        camo.location = pos
+        camo.rotation_mode = 'QUATERNION'
+        camo.rotation_quaternion = rot_quat
+        cam.angle = math.pi * fov / 180.0
+        cam.clip_start = 1e-3
+        cam.clip_end = 200.0
+
 
 def CamMotion_register():
     bpy.app.handlers.frame_change_pre.append(CamMotionFrameHandler)
